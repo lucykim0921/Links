0b16c055030e75b1520b3a0ebfe763306fa1c77f
lucykim0921 <kiml696@newschool.edu>
2024-02-07 11:23:54 -0500

    Update arena.js

--- arena.js
+++ arena.js
@@ -7,7 +7,7 @@ document.head.appendChild(markdownIt)
 
 
 // Okay, Are.na stuff!
-let channelSlug = 'typography-and-interaction-too' // The “slug” is just the end of the URL
+let channelSlug = 'lucykim0921' // The “slug” is just the end of the URL
 
 
 
